20f0b6255eb6db97b240bb5927c994e37d309842
vijay sahu <59651118+vijaysahuofficial@users.noreply.github.com>
2020-02-20 13:36:34 +0530

    Update js.js

--- js/js.js
+++ js/js.js
@@ -1,5 +1,6 @@
 
 $(document).ready(() => {
+    $('#error_msg').hide();
 
     $('.submit').on('click', (e) => {
         e.preventDefault();
@@ -8,6 +9,16 @@ $(document).ready(() => {
         let url = 'https://joeschmoe.io/api/v1/'+gender+'/'+name;
         console.log(name);
         console.log(gender);
+    
+        if (name == ''){
+            $('#error_msg').show();
+            return false
+        }
+        else{
+            $('#error_msg').hide();
+        }
+
+
         const output = `
        
 <div class="img-div">
@@ -24,11 +35,19 @@ $(document).ready(() => {
 
     const creditUrl = "https://www.facebook.com/vijaysahuofficialpage";
     const author = "Vijay Sahu";
-    const copyright = $('.x-copyright').text()
+    const copyright = $('.x-copyright').text();
     var credit = $('#credit').attr('href');
     if (credit != creditUrl && copyright != author) {
     alert("Created By Vijay Sahu")
     window.open(creditUrl)
-   }
+   };
+
+$('#new').on('click', ()=>{
+
+    location.reload(true);
+
+
+})
+
 
 })
